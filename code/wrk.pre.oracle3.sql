c209b4e175c23f8480bba66eb00a293f5a17af9f
blkangel <blkangel@mail.ru>
2017-05-04 23:11:47 +0300

    first commit
    first commit

---
+++ code/wrk.pre.oracle3.sql
@@ -0,0 +1,7 @@
+set define &
+
+-- login as sys
+
+create tablespace lz datafile '/opt/oracle/oradata/lz/omis_smp.dbf' SIZE 256M AUTOEXTEND ON NEXT 256M MAXSIZE UNLIMITED;
+create user lz identified by lZpa$$ default tablespace lz temporary tablespace temp;
+grant connect, resource, CREATE DATABASE LINK to lz;
