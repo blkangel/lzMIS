a2b262e04b7aec6bc81f359ed1c7682aa5611bc0
Vladimir Lebedev <vlebedev@emcmos.ru>
2017-05-16 09:24:19 +0300

    views users

--- code/wrk.pre.server.sql
+++ code/wrk.pre.server.sql
@@ -5,3 +5,4 @@ set define &
 create tablespace lz datafile '/opt/oracle/oradata/lz/lz001.dbf' SIZE 256M AUTOEXTEND ON NEXT 256M MAXSIZE UNLIMITED;
 create user lz identified by lZpa$$ default tablespace lz temporary tablespace temp;
 grant connect, resource, CREATE DATABASE LINK to lz;
+grant create view to lz;
